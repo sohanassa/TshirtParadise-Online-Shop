965e76acb676050370b8ded4eba0ebf79e702c5d
Btry123 <55946999+Btry123@users.noreply.github.com>
2022-06-21 15:05:48 +0300

    Products page and theme scc

--- products.php
+++ products.php
@@ -1,4 +1,6 @@
-<?php include('proccess.php') ?>
+<?php include('proccess.php') ?> 
+ 
+
 <!doctype html>
 <html lang="en">
 
@@ -6,7 +8,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Online Shop.">
-
+  <link rel="stylesheet" href="Productpagetheme.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
   <title>Home Page</title>
@@ -47,11 +49,33 @@
                 <a class="nav-link" href="register.php">Register</a>
               </li>
 
+              <div class="card">
+
             </ul>
         </div>
       </div>
     </nav>
+  </div>
+
+
   </section>
+
+  <div class="newdiv">
+    <img src="example.jpg" alt="Denim Jeans" style="width:50px;height:50px;">
+    <h1>Tailored Jeans</h1>
+    <p class="price">$19.99</p>
+    <p>Some text about the jeans..</p>
+    
+    <!-- up and down button -->
+    <input type="number"
+    min="0"
+    step="1"
+    value="0">
+    
+    <p class="newp"><button class="newbutton">Add to Cart</button></p>
+    
+  </div>
+
   <!-- Completed -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
