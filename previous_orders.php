67ebe4885548699094d4f5f85df6277050bed99e
ChenWou <maximecheniour@example.com>
2022-06-23 19:36:35 +0200

    Previous_Orders_PHP

--- previous_orders.php
+++ previous_orders.php
@@ -55,81 +55,38 @@
   </section>
 
   <div class="small-container cart-page">
-    <table>
+  <table>
+    <thead>
       <tr>
-        <th class="cart-type">Product</th>
+        <th class="cart-type">Order number</th>
         <th class="cart-type">Quantity</th>
-        <th class="cart-type">Subtotal</th>
+        <th class="cart-type">Total</th>
+        <th class="cart-type">Order again</th>
       </tr>
+    </thead>
+    <?php
+    $id = $_SESSION['id'];
+    $stmt = $db->prepare("SELECT * FROM order o, order_products p WHERE user_id = '$id' AND o.product_id = p.product_id");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()) :
+    ?>
       <tr>
         <td>
-          <div class="cart-info">
-            <img src="images/notRedTshirt.jpg" alt="">
-            <div>
-              <p>White Tshirt</p>
-              <small>Price: $50.00</small>
-              <br>
-              <a href="" class="cart-remove">Remove</a>
-            </div>
-          </div>
-        </td>
-        <td><input type="number" value="1"></td>
-        <td>$50.99</td>
-      </tr>
-      <tr>
+          <p><?= $row['order_id'] ?></p>
         <td>
-          <div class="cart-info">
-            <img src="images/notRedTshirt.jpg" alt="">
-            <div>
-              <p>White Tshirt</p>
-              <small>Price: $50.00</small>
-              <br>
-              <a href="" class="cart-remove">Remove</a>
-            </div>
-          </div>
+        <p><?= $row['quantity'] ?></p>
         </td>
-        <td><input type="number" value="1"></td>
-        <td>$50.99</td>
-      </tr>
-      <tr>
         <td>
-          <div class="cart-info">
-            <img src="images/notRedTshirt.jpg" alt="">
-            <div>
-              <p>White Tshirt</p>
-              <small>Price: $50.00</small>
-              <br>
-              <a href="" class="cart-remove">Remove</a>
-            </div>
-          </div>
+          <small>€<?= $row['total_price'] ?></small>
         </td>
-        <td><input type="number" value="1"></td>
-        <td>$50.99</td>
+        <td>
+          <!--ADD THE BUY AGAIN BUTTON-->
+        </td>
       </tr>
-    </table>
-
-    <div class="total-price">
-      <table>
-        <tr>
-          <td>Subtotal</td>
-          <td>$150.00</td>
-        </tr>
-        <tr>
-          <td>Tax</td>
-          <td>$35.00</td>
-        </tr>
-        <tr>
-          <td>Total</td>
-          <td>$185.00</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td> 
-            <button type="button">Order</button> 
-          </td>
-        </tr>
-      </table>
-    </div>
+      <br>
+    <?php endwhile; ?>
+  </table>
 
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
